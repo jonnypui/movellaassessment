01da490b7e4ee2afa136967b3fee4bff29faed17
jonnypui <43037228+jonnypui@users.noreply.github.com>
2023-03-02 11:16:57 -0500

    Update listofpods.ps1

--- listofpods.ps1
+++ listofpods.ps1
@@ -1,3 +1,4 @@
 kubectl get pods
 kubectl logs kubernetesdeployment-95dd87795-7qwnw
 pause
+<#As I am operating in a Windows environment, Task 5D would be repeated via Task Scheduler in 5 minute intervals.#>
